85cd1a736f63f9f6434db8409e7af33989b983c6
20171105115 <543065498@qq.com>
2020-07-27 23:35:26 +0800

    o2o

--- src/main/java/com/imooc/o2o/web/shopadmin/ProductManagementController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ProductManagementController.java
@@ -1,18 +1,33 @@
 package com.imooc.o2o.web.shopadmin;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.imooc.o2o.dto.ImageHolder;
 import com.imooc.o2o.dto.ProductCategoryExecution;
+import com.imooc.o2o.dto.ProductExecution;
+import com.imooc.o2o.dto.ShopExecution;
+import com.imooc.o2o.entity.Product;
 import com.imooc.o2o.entity.ProductCategory;
 import com.imooc.o2o.entity.Shop;
 import com.imooc.o2o.enums.ProductCategoryStateEnum;
+import com.imooc.o2o.enums.ProductStateEnum;
 import com.imooc.o2o.service.ProductCategoryService;
+import com.imooc.o2o.service.ProductService;
+import com.imooc.o2o.util.CodeUtil;
+import com.imooc.o2o.util.HttpServletRequestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +38,11 @@ public class ProductManagementController {
 
     @Autowired
     private ProductCategoryService productCategoryService;
+    @Autowired
+    private ProductService productService;
+
+
+    private static final  int MAXIMAGECOUNT = 6;
 
     /**
      * 删除商品类别
@@ -117,6 +137,91 @@ public class ProductManagementController {
             modelMap.put("success",false);
             modelMap.put("errMsg","请至少输入一个新的分类");
         }
+        return modelMap;
+    }
+
+    /**
+     * 添加商品
+     * 一、验证码校验
+     * 二、JSON字符串转化
+     * 三、缩略图接收并校验并操作
+     * 四、详情图接收并校验并操作
+     * 五、调用service层方法
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/addproduct", method = RequestMethod.POST)
+    @ResponseBody
+    private Map<String, Object> addProduct(HttpServletRequest request) {
+        Map<String,Object> modelMap = new HashMap<>();
+        if (!CodeUtil.checkVerifyCode(request)) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "请输入验证码");
+            return modelMap;
+        }
+
+        String productStr = HttpServletRequestUtil.getString(request,"productStr");
+        //接收参数
+        ObjectMapper mapper = new ObjectMapper();
+        Product product;
+        try {
+            product = mapper.readValue(productStr,Product.class);
+        } catch (JsonProcessingException e) {
+            modelMap.put("success",false);
+            modelMap.put("errMsg",e.getMessage());
+            return modelMap;
+        }
+        //获取文件
+        ImageHolder thumbnail;
+        List<ImageHolder> productImgList;
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(
+                request.getSession().getServletContext());
+        if (commonsMultipartResolver.isMultipart(request)){
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest)request;
+            try {
+                //取出文件流并且存放在文件封装类中(详情图)
+                CommonsMultipartFile multipartFile = (CommonsMultipartFile)multipartRequest.getFile("thumbnail");
+                thumbnail = new ImageHolder(multipartFile.getOriginalFilename(), multipartFile.getInputStream());
+                //取出文件流存放在List中
+                productImgList =  new ArrayList<>();
+                for (int i=0;i<MAXIMAGECOUNT;i++){
+                    CommonsMultipartFile productImgFile = (CommonsMultipartFile)multipartRequest.getFile("productImg"+i);
+                    if (productImgFile != null){//若不为空，则取出来
+                        ImageHolder productImg = new ImageHolder(productImgFile.getOriginalFilename(), productImgFile.getInputStream());
+                        productImgList.add(productImg);
+                    }else {//为空就跳出，说明已经循环完了
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                modelMap.put("success",false);
+                modelMap.put("errMsg",e.getMessage());
+                return modelMap;
+            }
+        }else {
+            modelMap.put("success",false);
+            modelMap.put("errMsg","店铺图片不能为空");
+            return modelMap;
+        }
+        //处理参数
+
+        try {
+            Shop shop = (Shop)request.getSession().getAttribute("currentShop");
+            product.setShop(shop);
+            ProductExecution pe = productService.addProduct(product, thumbnail, productImgList);
+            if (pe.getState() == ProductStateEnum.SUCCESS.getState()){
+                modelMap.put("success",true);
+            }else {
+                modelMap.put("success",false);
+                modelMap.put("errMsg","添加失败");
+            }
+        }catch (Exception e){
+            modelMap.put("success",false);
+            modelMap.put("errMsg","添加失败");
+            return modelMap;
+        }
+
+        //返回响应
         return modelMap;
     }
 }
---
+++ src/main/webapp/resources/js/shop/productoperation.js
@@ -0,0 +1,126 @@
+$(function () {
+    var productId = getQueryString('productId');
+    //获取商品列表
+    var getCategoryUrl = '/o2o/shopadmin/getproductcategorylist';
+    //获取商品原来信息
+    var getProductUrl;
+    //是否为修改
+    var Flag = false;
+    //新增时候的Url
+    var addUrl = '/o2o/shopadmin/addproduct';
+    //修改时候的Url
+    var editUrl = '/o2o/shopadmin/editproduct';
+
+    if (productId) {
+        Flag = true;
+    }
+
+
+
+    if (Flag) {//修改
+        getInfo(productId);
+    } else {//添加
+        getCategory();
+    }
+
+    function getInfo(productId) {
+        $.getJSON(getCategoryUrl,function (data) {
+            if (data.success){
+                var product = data.product;
+                $('#product-name').val(product.peoductName);
+                $('#product-desc').val(product.productDesc);
+                $('#priority').val(product.priority);
+                $('#normal-price').val(product.normalPrice);
+                $('#promotion-price').val(product.promotionPrice);
+
+                var categoryHtml = '';
+                var categoryList = data.productCategoryList;
+                var optionSelected = product.productCategory.productCategoryId;
+                categoryList.map(function (item) {
+                    var isSelect = optionSelected === item.productCategoryId ? 'selected':'';
+                    categoryHtml += '<option data-value="' + item.productCategoryId + isSelect
+                    + '">' + item.productCategoryName + '</option>';
+                });
+                $('#category').html(categoryHtml);
+            }
+        })
+    }
+
+    //获取分类
+    function getCategory() {
+        $.getJSON(getCategoryUrl, function (data) {
+            if (data.success) {
+                var category = data.productCategoryList;
+                var categoryHtml = '';
+                category.map(function (value) {
+                    categoryHtml += '<option data-value="' + value.productCategoryId
+                        + '">' + value.productCategoryName + '</option>'
+                });
+                $('#category').html(categoryHtml);
+            }
+        })
+    };
+
+    $('#submit').click(function () {
+        var url = Flag ? editUrl : addUrl;
+        var product = {};
+        if (Flag) {
+            product.productId = productId;
+        }
+        product.productName = $('#product-name').val();
+        product.productDesc = $('#product-desc').val();
+        product.priority = $('#priority').val();
+        product.normalPrice = $('#normal-price').val();
+        product.promotionPrice = $('#promotion-price').val();
+        product.productCategory = {
+            productCategoryId: $('#category').find('option').not(function () {
+                return !this.selected;
+            }).data('value')
+        }
+
+        //生成表单对象
+        var formData = new FormData();
+        var productStr = JSON.stringify(product);
+        formData.append("productStr", productStr);
+        var thumbnail = $('#small-img')[0].files[0];//因为是单个文件  所以获取[0]下标的元素
+        formData.append("thumbnail", thumbnail);
+        //遍历商品详情图控件，获取其中的文件流,存入formData对象中
+        $('.detail-img').map(function (index, item) {
+            if ($('.detail-img')[index].files.length > 0) {//判断该控件是否有文件
+                formData.append("productImg" + index,
+                    $('.detail-img')[index].files[0]);
+            }
+        })
+
+        //判断验证码
+        var verifyCodeActual = $('#j_captcha').val();
+        if (verifyCodeActual) {//如果已经输入，则提交
+            formData.append('verifyCodeActual', verifyCodeActual);
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: formData,
+                processData: false,// 告诉jQuery不要去处理发送的数据(必须设置)
+                contentType: false, // 告诉jQuery不要去设置Content-Type请求头（必须设置）
+                success: function (data) {
+                    if (data.success) {
+                        $.toast('提交成功');
+                    } else {
+                        $.toast('提交失败' + data.errMsg)
+                    }
+                    $('#captcha_img').click();
+                }
+            })
+        } else {
+            $.toast('请输入验证码')
+            return;
+        }
+    });
+
+    //判断详情图控件中的input是否是最后一个，不是的话则➕1
+    $('.detail-img-div').on('click', '.detail-img:last-child', function () {
+        if ($('.detail-img').length < 6) {
+            $('#detail-img').append('<input type="file" class="detail-img">');//追加到最后一个的后面
+        }
+    });
+})
